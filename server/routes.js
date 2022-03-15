1d5e746b58b02edcaf861ed2476c2f50f5e575ee
Igor <igor.gds.00@gmail.com>
2022-03-15 19:21:57 -0300

    Aula 1

---
+++ server/routes.js
@@ -0,0 +1,59 @@
+import config from "./config.js";
+import { Controller } from "./controller.js";
+import { logger } from "./util.js";
+
+const controller = new Controller()
+
+async function routes(req, res) {
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/') {
+    res.writeHead(302, {
+      'Location': config.location.home
+    })
+
+    return res.end()
+  }
+
+  if (method === 'GET' && url === '/home') {
+    const { stream } = await controller.getFileStream(config.pages.homeHTML)
+    return stream.pipe(res)
+  }
+
+  if (method === 'GET' && url === '/controller') {
+    const { stream } = await controller.getFileStream(config.pages.controllerHTML)
+    return stream.pipe(res)
+  }
+
+  if (method === 'GET') {
+    const { stream, type } = await controller.getFileStream(url)
+
+    const contentType = config.constants.CONTENT_TYPE[type]
+    if (contentType) {
+      res.writeHead(200, {
+        'Content-Type': contentType
+      })
+    }
+    return stream.pipe(res)
+  }
+
+  res.writeHead(404)
+  return res.end('hello')
+}
+
+function handlerError(error, res) {
+  if (error.message.includes('ENOENT')) {
+    logger.warn(`asset not found ${error.stack}`)
+    res.writeHead(404)
+    return res.end()
+  }
+
+  logger.error(`caught error on API ${error.stack}`)
+  res.writeHead(500)
+  return res.end()
+}
+
+export function handler(req, res) {
+  return routes(req, res)
+    .catch(error => handlerError(error, res))
+}
